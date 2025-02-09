38db352cbe07579fbc3e8f0bf167c85c9f745402
Dmitriy Ryazanov <dmitriy_ryazanov92@mail.ru>
2025-02-09 12:13:12 +0300

    dev

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    String folderPatch = "C:\\Users\\Дмитрий\\Desktop\\Новая папка";
+        File file = new File(folderPatch);
+        System.out.println(file.length());
+        System.out.println(getFolder(file));
+
+    }
+    public static long getFolder (File folder){
+        if (folder.isFile()){
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files)
+        {
+            sum += getFolder(file);
+        }
+return sum;
     }
 }
